6453d7c7bdc72e89c195265232b6ce3f16840482
Yee Mon Aung <yeemonaung.isure@icloud.com>
2026-05-18 12:36:35 +0100

    Add project files.

---
+++ RecordShopFrontEnd/Models/Album.cs
@@ -0,0 +1,13 @@
+﻿namespace RecordShopFrontEnd.Models
+{
+    public class Album
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Artist { get; set; }
+        public required string Genre { get; set; }
+        public int Year { get; set; }
+        public int Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
