fa8011b1488a4c9db2aef1f18321c0ec671fca61
Yee Mon Aung <yeemonaung.isure@icloud.com>
2026-05-19 11:44:56 +0100

    feat : create RecordDetails Component and Add functionality to the Save Changes button

--- RecordShopFrontEnd.Client/Program.cs
+++ RecordShopFrontEnd.Client/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Net.Http;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddScoped<HttpClient>();
+
 
 await builder.Build().RunAsync();
