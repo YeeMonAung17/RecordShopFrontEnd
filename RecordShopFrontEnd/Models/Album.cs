71d292c5284ac46e1802a20b6912edcc74970707
Yee Mon Aung <yeemonaung.isure@icloud.com>
2026-05-19 15:55:32 +0100

    Add data annotation for the class

--- RecordShopFrontEnd/Models/Album.cs
+++ RecordShopFrontEnd/Models/Album.cs
@@ -1,13 +1,27 @@
-﻿namespace RecordShopFrontEnd.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecordShopFrontEnd.Models
 {
     public class Album
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required.")]
         public required string Title { get; set; }
+
+        [Required(ErrorMessage = "Artist is required.")]
         public required string Artist { get; set; }
+
+        [Required(ErrorMessage = "Genre is required.")]
         public required string Genre { get; set; }
+
+        [Range(1900, 2100, ErrorMessage = "Year must be between 1900 and 2100.")]
         public int Year { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Price must be a non-negative value.")]
         public int Price { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Stock must be a non-negative value.")]
         public int Stock { get; set; }
     }
 }
